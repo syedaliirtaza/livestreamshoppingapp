37f4e55c480a9d9663a932184ef90ef9c1ead7e6
Ali Irtaza <ali.irtaza@nightingaletech.net>
2023-04-27 16:14:34 +0500

    added signup form

---
+++ pages/signup.js
@@ -0,0 +1,107 @@
+import React from "react";
+
+function signup() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <style>
+        {`
+        body {
+          background-image: linear-gradient(to bottom right, #FEC163, #DE4313);
+        }
+      `}
+      </style>
+      <div className="max-w-xl mx-auto bg-white shadow-md rounded px-8 py-6 sm:px-10 sm:py-8">
+        <form
+          action="https://send.pageclip.co/QL4zR6QGILiCb55sRChBydgPTNBNSXq1/signup"
+          class="pageclip-form"
+          method="post"
+        >
+          <div className="mb-6">
+            <h2 className="text-2xl font-bold mb-4 text-gray-800">
+              Baskett Waitlist
+            </h2>
+            <p className="text-gray-600">
+              Be the first to know when we launch our new product
+            </p>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="ali@gmail.com"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Ali"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="checkpoint"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Are you a seller or buyer?
+            </label>
+            <select
+              name="checkpoint"
+              id="checkpoint"
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            >
+              <option value="">Select an option</option>
+              <option>Seller</option>
+              <option>Buyer</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="city"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              City
+            </label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Lahore"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default signup;
